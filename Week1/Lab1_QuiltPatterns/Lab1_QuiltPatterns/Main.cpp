6732f35f0cc4ef90b2e3fc8917c4a71b0b256cf8
bijan102 <bfazeli1@gmail.com>
2016-09-02 10:06:00 -0700

    Created a project for the first lab

---
+++ Week1/Lab1_QuiltPatterns/Lab1_QuiltPatterns/Main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab1_QuiltPatterns
+//
+//  Created by Bijan Fazeli on 9/2/16.
+//  Copyright © 2016 Fazeli, Bijan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
